8ac848b23e5f1d1e02700f4a0be8fce2fd73af3c
ryomensukuna2003 <mshivanshu1264@gmail.com>
2024-10-07 00:13:49 +0530

    Added Practice Page

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -1,9 +1,235 @@
-"use client"
+"use client";
 
-const page = () => {
-  return (
-    <h1>All Problems page</h1>
-  )
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Input } from "@/components/ui/input";
+import { useUsername } from "../../components/usernameProvider";
+import { Checkbox } from "@/components/ui/checkbox"
+import { ModeToggle } from "../../components/ui/toggle";
+import { ChevronUp, ChevronDown } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+interface Problem {
+  contestId: number;
+  index: string;
+  name: string;
+  type: string;
+  rating: number;
+  tags: string[];
 }
 
-export default page
+export default function ProblemsPage() {
+  const [problems, setProblems] = useState<Problem[]>([]);
+  const [filteredProblems, setFilteredProblems] = useState<Problem[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [initialRating, setInitialFilter] = useState(800);
+  const [endingFilter, setEndingFilter] = useState(3200);
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+  const { username } = useUsername();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [displayedProblems, setDisplayedProblems] = useState<Problem[]>([]);
+  const contestsPerPage = 100;
+
+  useEffect(() => {
+    fetchProblems();
+  }, [username]);
+
+  const fetchProblems = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://codeforces.com/api/problemset.problems"
+      );
+      const data = await response.json();
+      if (data.status === "OK") {
+        setProblems(data.result.problems);
+        const filtered = data.result.problems.filter(
+          (problem: Problem) =>
+            problem.rating >= initialRating && problem.rating <= endingFilter
+        );
+        setFilteredProblems(filtered);
+        updateDisplayedProblems(filtered, 1);
+      } else {
+        throw new Error("Failed to fetch problems");
+      }
+    } catch (err) {
+      setError(
+        "An error occurred while fetching problems. Please try again later."
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const updateDisplayedProblems = (problems: Problem[], page: number) => {
+    const from = (page - 1) * contestsPerPage;
+    const to = from + contestsPerPage;
+    setDisplayedProblems(problems.slice(from, to));
+  };
+
+  useEffect(() => {
+    updateDisplayedProblems(filteredProblems, currentPage);
+  }, [currentPage, filteredProblems]);
+
+  const filterQuestions = () => {
+    const filtered = problems.filter(
+      (problem) =>
+        problem.rating >= initialRating && problem.rating <= endingFilter
+    );
+    setFilteredProblems(filtered);
+    setCurrentPage(1);
+    updateDisplayedProblems(filtered, 1);
+  };
+
+  const handleSort = () => {
+    const sorted = [...filteredProblems].sort((a, b) => {
+      if (sortOrder === "asc") {
+        return (a.rating || 0) - (b.rating || 0);
+      } else {
+        return (b.rating || 0) - (a.rating || 0);
+      }
+    });
+    setFilteredProblems(sorted);
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    updateDisplayedProblems(sorted, currentPage);
+  };
+
+  const totalPages = Math.ceil(filteredProblems.length / contestsPerPage);
+
+  const goToNextPage = () => {
+    setCurrentPage((page) => Math.min(page + 1, totalPages));
+  };
+
+  const goToPreviousPage = () => {
+    setCurrentPage((page) => Math.max(page - 1, 1));
+  };
+
+  return (
+    <div className="container mx-auto p-4 space-y-6 font-sans">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl flex-1 font-bold">Codeforces Problems</h1>
+        <Link className="mr-3" href="/">
+          <Button className="rounded" variant="outline">
+            Back to Dashboard
+          </Button>
+        </Link>
+        <ModeToggle />
+      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex justify-between items-center">
+            <span>Problem List</span>
+            <div className="flex items-center space-x-2">
+              <Input
+                type="text"
+                placeholder="Filter by rating"
+                value={initialRating}
+                onChange={(e) => setInitialFilter(Number(e.target.value))}
+                className="w-40"
+              />
+              <p className="text-base"> to </p>
+              <Input
+                type="text"
+                placeholder="Filter by rating"
+                value={endingFilter}
+                onChange={(e) => setEndingFilter(Number(e.target.value))}
+                className="w-40"
+              />
+              <Button onClick={filterQuestions}>Filter</Button>
+              <Button onClick={handleSort}>
+                Sort by Difficulty{" "}
+                {sortOrder === "asc" ? (
+                  <ChevronUp className="ml-2 h-4 w-4" />
+                ) : (
+                  <ChevronDown className="ml-2 h-4 w-4" />
+                )}
+              </Button>
+            </div>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          {isLoading ? (
+            <div className="text-center">Loading problems...</div>
+          ) : error ? (
+            <div className="text-center text-red-500">{error}</div>
+          ) : (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Name</TableHead>
+                  <TableHead>Rating</TableHead>
+                  <TableHead>Tags</TableHead>
+                  <TableHead>Solved</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {displayedProblems.map((problem) => (
+                  <TableRow key={`${problem.contestId}${problem.index}`}>
+                    <TableCell>
+                      <Link
+                        href={`https://codeforces.com/problemset/problem/${problem.contestId}/${problem.index}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hover:underline"
+                      >
+                        {problem.name}
+                      </Link>
+                    </TableCell>
+                    <TableCell>{problem.rating || "Unrated"}</TableCell>
+                    <TableCell>
+                      <div className="flex flex-wrap gap-1">
+                        {problem.tags.map((tag, index) => (
+                          <Badge key={index} variant="secondary">
+                            {tag}
+                          </Badge>
+                        ))}
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                    <Checkbox
+                      checked={true}
+                  // onCheckedChange={field.onChange}
+                />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          )}
+          <div className="flex justify-between items-center mt-4">
+            <Button
+              onClick={goToPreviousPage}
+              disabled={currentPage === 1}
+              variant="outline"
+            >
+              <ChevronLeft className="mr-2 h-4 w-4" /> Previous
+            </Button>
+            <span>
+              Page {currentPage} of {totalPages}
+            </span>
+            <Button
+              onClick={goToNextPage}
+              disabled={currentPage === totalPages}
+              variant="outline"
+            >
+              Next <ChevronRight className="ml-2 h-4 w-4" />
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
