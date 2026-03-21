5b105fb24e2bd26828b23a487d046a907eecfa57
ryomensukuna2003 <mshivanshu1264@gmail.com>
2026-03-21 16:57:21 +0530

    Removed Unused Packages

--- src/components/Providers/fetchAPI.tsx
+++ src/components/Providers/fetchAPI.tsx
@@ -16,23 +16,46 @@ export const useStore = create(
     allSubmissionsData: null,
     allRating: null,
     contestData: null,
+    codforcesContestData: null,
     isLoading: false,
     fetchData: async (username: string) => {
       set({ isLoading: true });
       try {
         await handleSaveUsername(username);
 
-        const [userInfoData, allSubmissionsData, allRating, contestData, UpcomingContestData, codforcesContestData, recentBlogs] = await Promise.all([
-          axios.get(`https://codeforces.com/api/user.info?handles=${username}`).then((res) => res.data),
-          axios.get(`https://codeforces.com/api/user.status?handle=${username}&from=1`).then((res) => res.data),
-          axios.get(`https://codeforces.com/api/user.rating?handle=${username}`).then((res) => res.data),
-          axios.get("https://codeforces.com/api/contest.list?gym=false").then((res) => res.data),
-          axios.get(`https://clist.by/api/v4/contest/?format=json&upcoming=true&username=Casper&api_key=${process.env.NEXT_PUBLIC_CLIST_API_KEY}&limit=100`).then((res) => res.data),
-          axios.get('https://codeforces.com/api/contest.list?gym=false').then((res) => res.data),
-          axios.get(`https://codeforces.com/api/recentActions?maxCount=100`).then((res) => res.data)
-        ]);
-        // Update the store state
-        set({ userInfoData, allSubmissionsData, allRating, contestData, UpcomingContestData, codforcesContestData, recentBlogs, isLoading: false });
+        const [userInfoData, allSubmissionsData, allRating, contestData, UpcomingContestData, recentBlogs] =
+          await Promise.all([
+            axios
+              .get(`https://codeforces.com/api/user.info?handles=${username}`)
+              .then((res) => res.data),
+            axios
+              .get(`https://codeforces.com/api/user.status?handle=${username}&from=1`)
+              .then((res) => res.data),
+            axios
+              .get(`https://codeforces.com/api/user.rating?handle=${username}`)
+              .then((res) => res.data),
+            axios
+              .get("https://codeforces.com/api/contest.list?gym=false")
+              .then((res) => res.data),
+            axios
+              .get(
+                `https://clist.by/api/v4/contest/?format=json&upcoming=true&username=Casper&api_key=${process.env.NEXT_PUBLIC_CLIST_API_KEY}&limit=100`
+              )
+              .then((res) => res.data),
+            axios
+              .get(`https://codeforces.com/api/recentActions?maxCount=100`)
+              .then((res) => res.data),
+          ]);
+        set({
+          userInfoData,
+          allSubmissionsData,
+          allRating,
+          contestData,
+          codforcesContestData: contestData,
+          UpcomingContestData,
+          recentBlogs,
+          isLoading: false,
+        });
       } catch (error) {
         console.error("Failed to fetch data:", error);
         set({ isLoading: false, userInfoData: "Username is not Valid" });
